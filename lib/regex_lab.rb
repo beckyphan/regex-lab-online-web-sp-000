61ce56ff8db539070a65fb6cf54b1dfd791c21da
Becky Phan <beckyphan@live.com>
2019-05-23 03:42:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if text.match(/\d{10}/) == nil
+  if text.match(/\b\d{3}+\b+\S+\d+\b/) == nil
     return false 
   else
     return true 
